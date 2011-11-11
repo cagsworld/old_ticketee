0574d1f28e18d5144cab0a43e8a4e623231a0ce2
Carl Graff <cagraff@cox.net>
2011-11-11 11:52:09 -0800

    Added tite functionality for show page

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -47,3 +47,12 @@ Then /^(?:|I )should see "([^"]*)"$/ do |text|
     assert page.has_content?(text)
   end
 end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
