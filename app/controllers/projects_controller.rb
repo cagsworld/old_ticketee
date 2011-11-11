0383936f4ab57f4138365970c8c041b8f5a76fa8
Carl Graff <cagraff@cox.net>
2011-11-11 14:03:43 -0800

    Add vaidation to ensure names are speified when creating projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,10 +6,14 @@ class ProjectsController < ApplicationController
 	end
 	def create
 		@project = Project.new(params[:project])
-		@project.save
-		flash[:notice] = "Project has been created."
-		redirect_to @project
-	end	
+		if @project.save
+			flash[:notice] = "Project has been created."
+			redirect_to @project
+		else
+			flash[:alert] = "Project has not been created."
+			render :action => "new"
+		end
+	end
 	def show
 		@project = Project.find(params[:id])
 	end
